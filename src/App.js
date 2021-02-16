7b65558782e9df1e376ac1d2577459b8ae94e5b2
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2021-02-15 21:16:58 -0300

    adding behavior add todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import TodoList from './TodoList'
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
   const [todos, setTodos] = useState([])
+  
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if(name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
       <TodoList todos = {todos}/>
-      <input type="text" />
-      <button>Add todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add todo</button>
       <button>Clear Complete</button>
       <div>0 left to do</div>
     </>
