f075cf7b3c4a61efd4f114f3f874abf2d18ee0bd
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2021-02-16 22:23:14 -0300

    added antd comps, must check input later

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, {useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList'
 import {v4 as uuidv4} from 'uuid'
+import 'antd/dist/antd.css'
+import { Button, Input, message} from 'antd'
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [inputText, setInputText]=useState({
+    input: ''
+  });
   const todoNameRef = useRef()
 
   useEffect(() => {
@@ -26,24 +31,43 @@ function App() {
 
   function handleAddTodo(e){
     const name = todoNameRef.current.value
-    if(name === '') return
+    if(name === ''){
+      message.error("Must add a name")
+      return
+    } 
     setTodos(prevTodos => {
       return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
     })
     todoNameRef.current.value = null
+    message.success("Added Todo succesfully!")
   }
+
+  // function handleAddTodo2(e){
+  //   const name = inputText
+  //   if(name === ''){
+  //     message.error("Must add a name")
+  //     return
+  //   }
+  //   setTodos(prevTodos => {
+  //     return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+  //   })
+  //   inputText.value = null
+  //   message.success("Added Todo succesfully!") 
+  // }
 
   function handleClearTodos(){
     const newTodos = todos.filter(todo => !todo.complete)
     setTodos(newTodos)
+    message.success("Cleared completed Todos!")
   }
 
   return (
     <>
       <TodoList todos = {todos} toggleTodo = {toggleTodo}/>
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add todo</button>
-      <button onClick={handleClearTodos}>Clear Complete</button>
+      {/* <Input placeholder = "sarasa" name="input"/> */}
+      <input ref={todoNameRef} />
+      <Button type="primary" onClick={handleAddTodo}>Add todo</Button>
+      <Button onClick={handleClearTodos}>Clear Complete</Button>
       <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import 'antd/dist/antd.css'
+import {Checkbox} from 'antd'
 
 export default function Todo({todo, toggleTodo}) {
     function handleTodoClick(){
         toggleTodo(todo.id)
     }
+
+    function onChange(){
+        toggleTodo(todo.id)
+    }
+
     return (
         <div>
-            <label>
+            {/* <label>
                 <input type="checkbox" checked={todo.complete} onChange = {handleTodoClick}/>
                 {todo.name}
-            </label>
+            </label> */}
+            <Checkbox onChange={onChange}>{todo.name}</Checkbox>
         </div>
     )
 }
